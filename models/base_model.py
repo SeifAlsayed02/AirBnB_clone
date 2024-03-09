82b8021ebc9c2893932a7cee8708e29d7b64790b
Ahmed Gaber <ahmedgaber312001@gmail.com>
2024-03-09 17:44:39 +0200

    fix *args & **kwargs bugs

--- models/base_model.py
+++ models/base_model.py
@@ -15,7 +15,7 @@ class BaseModel:
     base class for the project
     """
 
-    def __init__(*args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """
         The constructor of the class BaseModel
         """
@@ -24,8 +24,8 @@ class BaseModel:
                 if key == '__class__':
                     continue
                 if key == "updated_at" or key == "created_at":
-                    str_value = datetime.fromisoformat(value)
-                setattr(self, key, str_value)
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
             return
 
         self.id = str(uuid.uuid4())
@@ -56,3 +56,46 @@ class BaseModel:
         dict['updated_at'] = dict['updated_at'].isoformat()
 
         return dict
+
+
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+print("--")
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+print("--")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("--")
+print(my_model is my_new_model)
+
+# guillaume@ubuntu:~/AirBnB$ ./test_base_model_dict.py
+# 56d43177-cc5f-4d6c-a0c1-e167f8c27337
+# [BaseModel] (56d43177-cc5f-4d6c-a0c1-e167f8c27337) {'id': '56d43177-cc5f-4d6c-a0c1-e167f8c27337', 'created_at': datetime.datetime(2017, 9, 28, 21, 3, 54, 52298), 'my_number': 89, 'updated_at': datetime.datetime(2017, 9, 28, 21, 3, 54, 52302), 'name': 'My_First_Model'}
+# <class 'datetime.datetime'>
+# --
+# {'id': '56d43177-cc5f-4d6c-a0c1-e167f8c27337', 'created_at': '2017-09-28T21:03:54.052298', '__class__': 'BaseModel', 'my_number': 89, 'updated_at': '2017-09-28T21:03:54.052302', 'name': 'My_First_Model'}
+# JSON of my_model:
+#     id: (<class 'str'>) - 56d43177-cc5f-4d6c-a0c1-e167f8c27337
+#     created_at: (<class 'str'>) - 2017-09-28T21:03:54.052298
+#     __class__: (<class 'str'>) - BaseModel
+#     my_number: (<class 'int'>) - 89
+#     updated_at: (<class 'str'>) - 2017-09-28T21:03:54.052302
+#     name: (<class 'str'>) - My_First_Model
+# --
+# 56d43177-cc5f-4d6c-a0c1-e167f8c27337
+# [BaseModel] (56d43177-cc5f-4d6c-a0c1-e167f8c27337) {'id': '56d43177-cc5f-4d6c-a0c1-e167f8c27337', 'created_at': datetime.datetime(2017, 9, 28, 21, 3, 54, 52298), 'my_number': 89, 'updated_at': datetime.datetime(2017, 9, 28, 21, 3, 54, 52302), 'name': 'My_First_Model'}
+# <class 'datetime.datetime'>
+# --
+# False
