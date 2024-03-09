5ce951c15b886b9949d0d482e32c4d1cf23b33d5
Ahmed Gaber <ahmedgaber312001@gmail.com>
2024-03-09 17:38:42 +0200

    add *args & **kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -15,10 +15,19 @@ class BaseModel:
     base class for the project
     """
 
-    def __init__(self):
+    def __init__(*args, **kwargs):
         """
         The constructor of the class BaseModel
         """
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == "updated_at" or key == "created_at":
+                    str_value = datetime.fromisoformat(value)
+                setattr(self, key, str_value)
+            return
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
